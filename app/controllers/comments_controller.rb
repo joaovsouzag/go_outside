b80d5be0d7ea809bb332549280111f8a7a5b2dbc
Barbara Carloni <barbara-carloni@hotmail.com>
2022-11-30 16:11:44 -0300

    controller and view images

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,5 @@
+class CommentsController < ApplicationController
+  skip_after_action :verify_policy_scoped, only: :index
+  def index
+  end
+end
---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,5 @@
+class VideosController < ApplicationController
+  skip_after_action :verify_policy_scoped, only: :index
+  def index
+  end
+end
