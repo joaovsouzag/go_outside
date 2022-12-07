033af4faa6a45271cb7cc23c12a8e83ba857375d
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-12-07 16:37:26 -0300

    grid photos working

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,5 +2,6 @@ class ImagesController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
   def index
     @location = Location.find(params[:location_id])
+    @feedbacks = Feedback.where(location_id: params[:location_id])
   end
 end
